3ddc105dc88c6679c0cb87bbb3442a581a4f7d29
AnthonyBotha <anthony.botha@outlook.com>
2024-04-15 21:31:06 -0700

    Phase 2

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,19 @@
--- Your code here 
+-- Your code here --
+DROP TABLE IF EXISTS customer;
+
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE customer (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40) NOT NULL,
+    phone INTEGER(10) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    points INTEGER NOT NULL DEFAULT 5,
+    created_at CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_orders (
+    id INTEGER PRIMARY KEY,
+    is_redeemed BOOLEAN DEFAULT "not redeemed",
+    ordered_at CURRENT_TIMESTAMP
+);
